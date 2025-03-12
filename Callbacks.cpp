85b0595ad594dd08a5a028e295cc67eb255431e9
Samuel <samue.acuna-g@mail.escuelaing.edu.co>
2025-03-12 10:24:03 -0500

    Con comentarios

--- Callbacks.cpp
+++ Callbacks.cpp
@@ -1,19 +1,20 @@
+// Hecho por: Samuel Acuña y Carolina García
 #include <iostream>
 #include <string>
 using namespace std;
 
 // Callback functions for different operations
-int add(int a, int b)
+int add(int a, int b) // suma
 {
     return a + b;
 }
 
-int multiply(int a, int b)
+int multiply(int a, int b) // multiplicación
 {
     return a * b;
 }
 
-int subtract(int a, int b)
+int subtract(int a, int b) // resta
 {
     return a - b;
 }
@@ -21,40 +22,40 @@ int subtract(int a, int b)
 // Function that performs an operation using a callback
 void performOperation(int (*callback)(int, int), int x, int y)
 {
-    cout << "The result of the operation is: " << callback(x, y) << endl;
+    cout << "The result of the operation is: " << callback(x, y) << endl; // imprime el resultado de la operación
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) // argc es el número de argumentos que se pasan y argv es un array de punteros a los argumentos
 {
     if (argc != 4)
     {
-        cerr << "Usage: " << argv[0] << " <num1> <operation> <num2>" << endl;
+        cerr << "Usage: " << argv[0] << " <num1> <operation> <num2>" << endl; // imprime el mensaje de error
         return 1;
     }
 
-    int num1 = stoi(argv[1]);
-    int num2 = stoi(argv[3]);
+    int num1 = stoi(argv[1]); // stoi convierte un string a un entero
+    int num2 = stoi(argv[3]); // stoi convierte un string a un entero
     char op = argv[2][0];
 
     // Passing different functions as callbacks
     if (op == '+')
     {
         cout << "Addition: ";
-        performOperation(add, num1, num2);
+        performOperation(add, num1, num2); // llama a la función add
     }
-    else if (op == 'x')
+    else if (op == 'x') // 'x' is used to represent multiplication no se usa '*' porque es un caracter especial
     {
         cout << "Multiplication: ";
-        performOperation(multiply, num1, num2);
+        performOperation(multiply, num1, num2); // llama a la función multiply
     }
     else if (op == '-')
     {
         cout << "Subtraction: ";
-        performOperation(subtract, num1, num2);
+        performOperation(subtract, num1, num2); // llama a la función subtract
     }
     else
     {
-        cerr << "Invalid operation" << endl;
+        cerr << "Invalid operation" << endl; // imprime el mensaje de error
     }
 
     int (*operations[])(int, int) = {add, multiply, subtract};
@@ -62,7 +63,7 @@ int main(int argc, char *argv[])
     for (int i = 0; i < 3; ++i)
     {
         cout << "Operation " << i + 1 << ": ";
-        performOperation(operations[i], num1, num2);
+        performOperation(operations[i], num1, num2); // llama a las funciones del array
     }
 
     cout << argv << endl;
